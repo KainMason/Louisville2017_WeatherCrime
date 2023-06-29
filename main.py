86906e0a3d12142edb211e3f0f42cfcd7e869b44
Tyler Kain Mason <95332889+KainMason@users.noreply.github.com>
2023-06-29 15:05:14 -0400

    added gui

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Read the crime.csv file with specified data types and low_memory=False
 crime_data = pd.read_csv('crime2017.csv', dtype={'ID': str}, low_memory=False)
@@ -30,13 +32,25 @@ low_temp_counts = low_temp_data['CRIME_TYPE'].value_counts()
 # Combine the counts for each crime type
 combined_counts = pd.concat([high_temp_counts, low_temp_counts], axis=1, keys=['High Temperature', 'Low Temperature'])
 
-# Plot the bar chart
-plt.figure(figsize=(10, 6))  # Create a figure with specified size
-combined_counts.plot(kind='bar')  # Plot the combined counts as a bar chart
-plt.xlabel('Crime Type')  # Set the x-axis label
-plt.ylabel('Number of Incidents')  # Set the y-axis label
-plt.title('Crime Incidents by Type and Temperature in 2017')  # Set the chart title
-plt.xticks(rotation=90)  # Rotate the x-axis tick labels for better readability
-plt.legend()  # Show the legend
+# Create a Tkinter window
+window = tk.Tk()
+window.title('Crime Incidents by Type and Temperature in 2017')
 
-plt.show()  # Display the chart
+# Create a Figure and Axes for the bar chart
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Plot the bar chart on the Figure and Axes
+combined_counts.plot(kind='bar', ax=ax)
+ax.set_xlabel('Crime Type')
+ax.set_ylabel('Number of Incidents')
+ax.set_title('Crime Incidents by Type and Temperature in 2017')
+ax.tick_params(axis='x', rotation=90)
+ax.legend()
+
+# Create a FigureCanvasTkAgg widget and add it to the Tkinter window
+canvas = FigureCanvasTkAgg(fig, master=window)
+canvas.draw()
+canvas.get_tk_widget().pack()
+
+# Run the Tkinter event loop
+tk.mainloop()
