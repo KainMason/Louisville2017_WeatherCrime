9855535e2d2c4ecd36c4c5dc0ce1f8db60ae44b3
Tyler Kain Mason <95332889+KainMason@users.noreply.github.com>
2023-07-06 15:14:12 -0400

    updating code. creating gui. cleaning data

--- main.py
+++ main.py
@@ -3,54 +3,104 @@ import matplotlib.pyplot as plt
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-# Read the crime.csv file with specified data types and low_memory=False
-crime_data = pd.read_csv('crime2017.csv', dtype={'ID': str}, low_memory=False)
+# Function to display the bar chart of crime types
+def show_crime_types_chart():
+    plt.figure(figsize=(10, 6))
+    crime_type_counts.plot(kind='bar')
+    plt.xlabel('Crime Type')
+    plt.ylabel('Count')
+    plt.title('Crime Types')
+    plt.show()
 
-# Read the weather.csv file
-weather_data = pd.read_csv('weather.csv')
+# Function to display the bar chart of crime types and temperature conditions
+def show_crimes_by_temperature_chart():
+    fig, ax = plt.subplots(figsize=(12, 6))
+    grouped_df.plot(kind='bar', ax=ax)
+    plt.xlabel('Crime Type')
+    plt.ylabel('Number of Crimes')
+    plt.title('Number of Crimes by Crime Type and Temperature Condition')
+    plt.legend(['Below 50', '50 and Above'])
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.show()
 
-# Convert the date columns to the appropriate data type
-crime_data['DATE_OCCURED'] = pd.to_datetime(crime_data['DATE_OCCURED']).dt.date
-weather_data['date'] = pd.to_datetime(weather_data['date']).dt.date
+# Function to display the pivot table and bar plot
+def show_crime_count_by_month():
+    pivot_table.plot(kind='bar', figsize=(12, 6))
+    plt.xlabel('Crime Type')
+    plt.ylabel('Count')
+    plt.title('Crime Count by Crime Type and Month')
+    plt.legend(title='Month', bbox_to_anchor=(1, 1))
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.show()
 
-# Merge the datasets based on the date column
-merged_data = pd.merge(crime_data, weather_data, left_on='DATE_OCCURED', right_on='date', how='inner')
+# Read the crime data from crime2017.csv
+crime_df = pd.read_csv('crime2017.csv', dtype={'ZIP_CODE': str})
 
-# Filter the merged dataset for the year 2017
-merged_data_2017 = merged_data[pd.to_datetime(merged_data['DATE_OCCURED']).dt.year == 2017]
+# Read the weather data from weather.csv
+weather_df = pd.read_csv('weather.csv')
+# Clean the crime data
+crime_df = pd.read_csv('crime2017.csv', dtype={'ZIP_CODE': str})
+crime_df = crime_df[crime_df['YEAR_OCCURED'] == 2017.0]
 
-# Separate the data into two groups based on temperature
-high_temp_data = merged_data_2017[merged_data_2017['high'] >= 50]
-low_temp_data = merged_data_2017[merged_data_2017['high'] < 50]
 
-# Calculate the count of each crime type for high temperatures (>= 50)
-high_temp_counts = high_temp_data['CRIME_TYPE'].value_counts()
 
-# Calculate the count of each crime type for low temperatures (< 50)
-low_temp_counts = low_temp_data['CRIME_TYPE'].value_counts()
+# Clean the crime data
+crime_df.dropna(subset=['date'], inplace=True)
+crime_df['date'] = pd.to_datetime(crime_df['date'])
+crime_df.drop_duplicates(inplace=True)
 
-# Combine the counts for each crime type
-combined_counts = pd.concat([high_temp_counts, low_temp_counts], axis=1, keys=['High Temperature', 'Low Temperature'])
+# Clean the weather data
+weather_df.dropna(subset=['date'], inplace=True)
+weather_df['date'] = pd.to_datetime(weather_df['date'])
+weather_df.drop_duplicates(inplace=True)
 
-# Create a Tkinter window
-window = tk.Tk()
-window.title('Crime Incidents by Type and Temperature in 2017')
+# Merge the crime and weather data on the common column 'date'
+combined_df = pd.merge(crime_df, weather_df, on='date')
 
-# Create a Figure and Axes for the bar chart
-fig, ax = plt.subplots(figsize=(10, 6))
+# Calculate crime type counts
+crime_type_counts = combined_df['CRIME_TYPE'].value_counts()
 
-# Plot the bar chart on the Figure and Axes
-combined_counts.plot(kind='bar', ax=ax)
-ax.set_xlabel('Crime Type')
-ax.set_ylabel('Number of Incidents')
-ax.set_title('Crime Incidents by Type and Temperature in 2017')
-ax.tick_params(axis='x', rotation=90)
-ax.legend()
+# Group the data by crime type and temperature condition
+grouped_df = combined_df.groupby(['CRIME_TYPE', combined_df['high'] >= 50]).size().unstack()
 
-# Create a FigureCanvasTkAgg widget and add it to the Tkinter window
-canvas = FigureCanvasTkAgg(fig, master=window)
-canvas.draw()
-canvas.get_tk_widget().pack()
+# Create a pivot table to summarize crime count by crime type and month
+pivot_table = crime_df.pivot_table(index='CRIME_TYPE', columns=crime_df['date'].dt.month_name(), aggfunc='size', fill_value=0)
 
-# Run the Tkinter event loop
-tk.mainloop()
+# Create the GUI window
+root = tk.Tk()
+root.title('Crime Data Visualization')
+root.geometry('800x600')
+
+# Create a label to explain the purpose of the program
+explanation_label = tk.Label(root, text='This program visualizes crime data')
+explanation_label.pack(pady=10)
+
+# Create buttons for each chart
+crime_types_button = tk.Button(root, text='Crime Types', command=show_crime_types_chart)
+crime_types_button.pack()
+
+crimes_by_temperature_button = tk.Button(root, text='Crimes by Temperature', command=show_crimes_by_temperature_chart)
+crimes_by_temperature_button.pack()
+crime_count_by_month_button = tk.Button(root, text='Crime Count by Month', command=show_crime_count_by_month)
+crime_count_by_month_button.pack()
+
+# Create a matplotlib figure and canvas for embedding plots in the GUI
+figure = plt.figure(figsize=(8, 6))
+canvas = FigureCanvasTkAgg(figure, master=root)
+canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+# Function to update the embedded plot in the GUI
+def update_plot(plot_func):
+    figure.clear()
+    plot_func()
+    canvas.draw()
+
+# Update the embedded plot when the corresponding button is pressed
+crime_types_button.config(command=lambda: update_plot(show_crime_types_chart))
+crimes_by_temperature_button.config(command=lambda: update_plot(show_crimes_by_temperature_chart))
+crime_count_by_month_button.config(command=lambda: update_plot(show_crime_count_by_month))
+
+# Display the GUI window
+root.mainloop()
